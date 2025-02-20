aa666bf1321362f041abee6e10a61ac1d8ede565
alumno <raulplaza208@gmail.com>
2025-02-20 19:30:35 +0100

    Terminado la primera parte del proyecto de angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,30 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// Ejercicio 1
+// export class AppComponent {
+//   nombre = 'Rodriguez Pablo';
+//   edad = 7;
+//   sueldos = [1700, 1600, 1900];
+// }
+
+// Ejercicio 2
+// export class AppComponent {
+//   contador = 1;
+//   incrementar() {
+//     this.contador++;
+//   }
+//   decrementar() {
+//     this.contador--;
+//   }
+// }
+
+// Ejercicio 3
+// export class AppComponent {
+//   nombre = "juan";
+// }
+
 export class AppComponent {
-  title = 'Angular2';
+  nombre = "";
+  apellido = "";
 }
